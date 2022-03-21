f69823c805a352c0283da8757c53f143ef878e82
NotEvil <worldwow225@gmail.com>
2022-03-21 17:48:31 +0400

    Updates

---
+++ precache-manifest.bd92b7866d86148f002170e92e5fb4ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d66487fa114562d5639577f719729d2",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "52f4d3d1ae900a1e2ea1",
+    "url": "/react-test/static/js/2.5112b54a.chunk.js"
+  },
+  {
+    "revision": "8d7bcc39f053b3f19437",
+    "url": "/react-test/static/js/main.cc9147a3.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  }
+]);
